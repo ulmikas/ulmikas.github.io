ea87a1bb30f38146e1cbe9de2dfcb9c6365e0289
kenway <kenway@ecwid.com>
2017-02-02 00:03:20 +0400

    add pages, make actual links

--- rapidspar.com/js/rapidspar.js
+++ rapidspar.com/js/rapidspar.js
@@ -1,32 +1,31 @@
 $(document).ready(function() {
     $("#contact_submit").click(function() {
-
         var proceed = true;
-        $("#contact_form input[required=true], #contact_form textarea[required=true]").each(function(){
-            $(this).css('border','none');
-            if(!$.trim($(this).val())){ //if this field is empty
-                $(this).css('border','2px solid red'); //change border color to red
+        $("#contact_form input[required=true], #contact_form textarea[required=true]").each(function() {
+            $(this).css('border', 'none');
+            if (!$.trim($(this).val())) { //if this field is empty
+                $(this).css('border', '2px solid red'); //change border color to red
                 proceed = false; //set do not proceed flag
             }
             //check invalid email
             var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-            if($(this).attr("type")=="email" && !email_reg.test($.trim($(this).val()))){
-                $(this).css('border','2px solid red'); //change border color to red
+            if ($(this).attr("type") == "email" && !email_reg.test($.trim($(this).val()))) {
+                $(this).css('border', '2px solid red'); //change border color to red
                 proceed = false; //set do not proceed flag
             }
         });
         if (proceed) {
             post_data = {
-                'name'     : $('input[name=name]').val(),
-                'email'    : $('input[name=email]').val(),
-                'message'  : $('textarea[name=message]').val()
+                'name': $('input[name=name]').val(),
+                'email': $('input[name=email]').val(),
+                'message': $('textarea[name=message]').val()
             };
             //Ajax post data to server
             $.post('src/contact.php', post_data, function(response) {
                 if (response.type == 'error') {
-                    output = '<div class="error">'+response.text+'</div>';
+                    output = '<div class="error">' + response.text + '</div>';
                 } else {
-                    output = '<div class="success">'+response.text+'</div>';
+                    output = '<div class="success">' + response.text + '</div>';
                     //reset values in all input fields
                     $("#contact_form  input[required=true], #contact_form textarea[required=true]").val('');
                     $("#contact_form #contact_body").slideUp(); //hide form after success
@@ -38,7 +37,7 @@ $(document).ready(function() {
     });
     //reset previously set border colors and hide all message on .keyup()
     $("#contact_form  input[required=true], #contact_form textarea[required=true]").keyup(function() {
-        $(this).css('border-color','');
+        $(this).css('border-color', '');
         $("#contact_results").slideUp();
     });
 });
