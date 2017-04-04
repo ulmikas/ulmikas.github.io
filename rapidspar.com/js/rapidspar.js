2280dbb96f0e6ec4a14742a6b5eb253090e63edd
kenway <kenway@ecwid.com>
2017-04-04 21:33:52 +0400

    fix form

--- rapidspar.com/js/rapidspar.js
+++ rapidspar.com/js/rapidspar.js
@@ -1,73 +1,79 @@
-$(document).ready(function() {
-    $("#contact_submit").click(function() {
-        var proceed = true;
-        $("#contact_form input[required=true], #contact_form textarea[required=true]").each(function() {
-            $(this).css('border', 'none');
-            if (!$.trim($(this).val())) { //if this field is empty
-                $(this).css('border', '2px solid red'); //change border color to red
-                proceed = false; //set do not proceed flag
-            }
-            //check invalid email
-            var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-            if ($(this).attr("type") == "email" && !email_reg.test($.trim($(this).val()))) {
-                $(this).css('border', '2px solid red'); //change border color to red
-                proceed = false; //set do not proceed flag
-            }
-        });
-        if (proceed) {
-            $(this).addClass('loading');
-            $(this).attr('disabled','disabled');
-            post_data = {
-                'name': $('input[name=name]').val(),
-                'email': $('input[name=email]').val(),
-                'phone': $('input[name=phone]').val(),
-                'company': $('input[name=company]').val(),
-                'message': $('textarea[name=message]').val()
-            };
-            //Ajax post data to server
-            $.post('contact.php', post_data, function(response) {
-                if (response.type == 'error') {
-                  output = '<div class="error">' + response.text + '</div>';
-                } else {
-                  $(this).removeClass('loading');
-                  $('#contacts .contact-us__success').fadeIn();
-                  setTimeout(function(){
-                    $('#contacts .contact-us__success').fadeOut();
-                  }, 3000);
-                    // output = '<div class="success">' + response.text + '</div>';
-                    // //reset values in all input fields
-                    // $("#contact_form  input[required=true], #contact_form textarea[required=true]").val('');
-                    // $("#contact_form #contact_body").slideUp(); //hide form after success
-                }
-                $("#contact_form #contact_results").hide().html(output).slideDown();
-            }, 'json');
+$(document).ready(function () {
+  $('#contact_submit').click(function () {
+    var proceed = true;
+    $('#contact_form input[required=true], #contact_form textarea[required=true]').each(function () {
+      $(this).css('border', 'none');
+      if (!$.trim($(this).val())) { //if this field is empty
+        $(this).css('border', '2px solid red'); //change border color to red
+        proceed = false; //set do not proceed flag
+      }
+      //check invalid email
+      var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+      if ($(this).attr('type') == 'email' && !email_reg.test($.trim($(this).val()))) {
+        $(this).css('border', '2px solid red'); //change border color to red
+        proceed = false; //set do not proceed flag
+      }
+    });
+    if (proceed) {
+      $(this).addClass('loading');
+      $(this).attr('disabled', 'disabled');
+      $(this).blur();
+      post_data = {
+        "name": $('input[name=name]').val(),
+        "email": $('input[name=email]').val(),
+        "phone": $('input[name=phone]').val(),
+        "company": $('input[name=company]').val(),
+        "message": $('textarea[name=message]').val()
+      };
+      //Ajax post data to server
+      $.post('contact.php', post_data, function (response) {
+        var output = '';
+        $('#contacts .contact-us__success, #contacts .contact-us__error').remove();
+        $('#contact_submit').removeClass('loading');
+        $('#contact_submit').removeAttr('disabled');
+
+        if (response.type == 'error') {
+          output = '<div class="contact-us__error" style="clear: both; margin-top: -20px; margin-bottom: 20px; color: red;">' + response.text + '</div>';
+        } else {
+          output = '<div class="contact-us__success">' + response.text + '</div>';
+          //reset values in all input fields
+          $('#contact_form  input, #contact_form textarea').val('');
         }
-        return false;
-    });
-    //reset previously set border colors and hide all message on .keyup()
-    $("#contact_form  input[required=true], #contact_form textarea[required=true]").keyup(function() {
-        $(this).css('border-color', '');
-        $("#contact_results").slideUp();
-    });
+        $('#contact_submit').before(output);
+        $('#contacts .contact-us__success').fadeIn();
+        setTimeout(function () {
+          $('#contacts .contact-us__success, #contacts .contact-us__remove').fadeOut(function(){
+            $('#contacts .contact-us__success').remove();
+          });
+        }, 3000);
+      }, 'json');
+    }
+    return false;
+  });
+  //reset previously set border colors and hide all message on .keyup()
+  $('#contact_form  input[required=true], #contact_form textarea[required=true]').keyup(function () {
+    $(this).css('border-color', '');
+    $('#contact_results').slideUp();
+  });
 
 
-    $( ".frame-1" ).on( "click", function() {
-        $('.s1').hide();
-        $(this).hide();
-        $('.firsframe').attr('src','anim/rapidspar-p1/rapidspar-p1.html');
-    });
-    $( ".frame-2" ).on( "click", function() {
-        $('.s2').hide();
-        $(this).hide();
-        $('.secondframe').attr('src','anim/rapidspar-p2/rapidspar-p2.html');
-    });
-    $( ".frame-3" ).on( "click", function() {
-        $('.s3').hide();
-        $(this).hide();
-        $('.threeframe').attr('src','anim/rapidspar-p3/rapidspar-p3.html');
-    });
+  $('.frame-1').on('click', function () {
+    $('.s1').hide();
+    $(this).hide();
+    $('.firsframe').attr('src', 'anim/rapidspar-p1/rapidspar-p1.html');
+  });
+  $('.frame-2').on('click', function () {
+    $('.s2').hide();
+    $(this).hide();
+    $('.secondframe').attr('src', 'anim/rapidspar-p2/rapidspar-p2.html');
+  });
+  $('.frame-3').on('click', function () {
+    $('.s3').hide();
+    $(this).hide();
+    $('.threeframe').attr('src', 'anim/rapidspar-p3/rapidspar-p3.html');
+  });
 
-    $("body").on("click", ".modal-backdrop, #myModal .close, #myModal .btn", function() {
-        $("#myModal iframe").attr("src", $("#myModal iframe").attr("src"));
-    });
+  $('body').on('click', '.modal-backdrop, #myModal .close, #myModal .btn', function () {
+    $('#myModal iframe').attr('src', $('#myModal iframe').attr('src'));
+  });
 });
